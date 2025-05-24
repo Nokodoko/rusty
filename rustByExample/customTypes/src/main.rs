c2f791e8fea2b4dfc5df48398d05f623acca9c01
Chris <cmonty614@gmail.com>
2025-05-24 01:49:52 -0400

    updates

--- rustByExample/customTypes/src/main.rs
+++ rustByExample/customTypes/src/main.rs
@@ -1,6 +1,63 @@
-//ENUMS
+//HACK:ENUMS
+enum Number {
+    Zero,
+    One,
+    Two,
+}
 
-//structs
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+enum Stage {
+    Beginner,
+    Advanced,
+}
+
+enum Role {
+    Student,
+    Teacher,
+}
+
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
+}
+
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers; //NOTE:type aliases
+
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
+    }
+}
+
+enum WebEvent {
+    PageLoad,
+    PageUnload,
+    KeyPress(char),
+    Paste(String),
+    Click { x: i64, y: i64 },
+}
+
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        WebEvent::KeyPress(c) => println!("Pressed {}", c),
+        WebEvent::Paste(s) => println!("Paseted: {}", s),
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}", x, y);
+        }
+    }
+}
+
+//HACK:STRUCTS
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -39,6 +96,45 @@ impl Rectangle {
 }
 
 fn main() {
+    //NOTE:enums
+
+    println!("Zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+
+    use crate::Role::*;
+    use crate::Stage::{Advanced, Beginner};
+
+    let stage = Beginner;
+    let role = Student;
+
+    match stage {
+        Beginner => println!("Beginners are starting their learning journey"),
+        Advanced => println!("Advanced learners are mastering their subjedts ..."),
+    }
+
+    match role {
+        Student => println!("Stednts are aquiring knowledge"),
+        Teacher => println!("Teachers are spreading knowledge"),
+    }
+
+    let pressed = WebEvent::KeyPress('x');
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+
+    let x = Operations::Add;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+
+    //NOTE:structs
     let name = String::from("Peter");
     let age = 27;
     let peter = Person { name, age };
